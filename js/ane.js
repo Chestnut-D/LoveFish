47845b5b7479e2b0bdc7889db23e4ecf92c5e212
Chestnut-D <1143812160@qq.com>
2020-07-12 18:24:40 +0800

    hh

--- js/ane.js
+++ js/ane.js
@@ -33,15 +33,11 @@ aneObj.prototype.draw = function () {
     for (var i = 0; i < this.num; i++) {
         //使用到的API：beginPath,moveTo,lineTo,stroke,strokeStyle,lineWidth,lineCap,globalAlpha透明度
         ctx2.beginPath();
-        //二次贝塞尔曲线
-        //起始点，控制点，终点
-        
-        /* 曲线弯曲方向: 控制点不会显示出来,曲线弯曲方向,朝向 控制点的坐标位置 
-           改变曲线方向 = 改变 控制点的坐标 (不改变 起始点和终点时)
-           改变曲线的形状: 改变 起始点,终点,控制点坐标,都可以 改变曲线的形状*/
-
+        /* 贝塞尔曲线
+        曲线弯曲方向: 控制点不会显示出来,曲线弯曲方向,朝向 控制点的坐标位置 
+        改变曲线方向 = 改变 控制点的坐标 (不改变 起始点和终点时)
+        改变曲线的形状: 改变 起始点,终点,控制点坐标,都可以 改变曲线的形状*/
         ctx2.moveTo(this.rootx[i], canHeight);//起始点
-
         this.headx[i] = this.rootx[i] + l * this.amp[i];
         ctx2.quadraticCurveTo(this.rootx[i], canHeight - 100, this.headx[i], this.heady[i]); //海葵高度（控制点的x y ， 终点的x y）
         ctx2.stroke();
